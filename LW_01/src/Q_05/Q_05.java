6cc18afb7c50d09d89d7dff5970757b81070d3c2
JanithHarshana <muthukuda.janith@gmail.com>
2025-03-09 20:06:46 +0530

    Update Q_05.java

--- LW_01/src/Q_05/Q_05.java
+++ LW_01/src/Q_05/Q_05.java
@@ -4,10 +4,10 @@ import java.util.*;
 
 public class Q_05 {
     public static void main(String[] args) {
-        Date today;
+        Date date;
         SimpleDateFormat sdf;
-        today = new Date();
+        date = new Date();
         sdf = new SimpleDateFormat("EEEE, MMMM dd, yyyy");
-        System.out.println("Today's date is: " + sdf.format(today));
+        System.out.println("Today's date is: " + sdf.format(date));
     }
 }
